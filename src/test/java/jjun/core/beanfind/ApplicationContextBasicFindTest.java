7308e3a4714e4fd8b4baf395b49ddb857198bad3
jun <jun020216@naver.com>
2022-07-06 21:50:58 +0900

    Spring Bean FindByName/Type

--- src/test/java/jjun/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/jjun/core/beanfind/ApplicationContextBasicFindTest.java
@@ -1,37 +1,50 @@
 package jjun.core.beanfind;
 
 import jjun.core.AppConfig;
+import jjun.core.member.MemberService;
+import jjun.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextBasicFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
-    @DisplayName("모든 빈 출력하기")
-    void findAllBean() {
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName + " object = " + bean);
-        }
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
     
     @Test
-    @DisplayName("애플리케이션 빈 출력하기")
-    void findApplicationBean() {
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
-
-            
-            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
-                Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("name = " + beanDefinitionName + " object = " + bean);
-            }
-        }
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+//        ac.getBean("xxxx", MemberService.class);
+        //MemberService xxxx = ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxx", MemberService.class));
+    }
+
 }
