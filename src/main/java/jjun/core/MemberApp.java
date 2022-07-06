8d78025f14114924967be40b387e809a91424353
jun <jun020216@naver.com>
2022-07-06 21:27:43 +0900

    [FEAT] find Spring Beans

--- src/main/java/jjun/core/MemberApp.java
+++ src/main/java/jjun/core/MemberApp.java
@@ -4,12 +4,20 @@ import jjun.core.member.Grade;
 import jjun.core.member.Member;
 import jjun.core.member.MemberService;
 import jjun.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
+        /*
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
+        */
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/jjun/core/OrderApp.java
+++ src/main/java/jjun/core/OrderApp.java
@@ -7,14 +7,23 @@ import jjun.core.member.MemberServiceImpl;
 import jjun.core.order.Order;
 import jjun.core.order.OrderService;
 import jjun.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
+        /*
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
         OrderService orderService = appConfig.orderService();
+        */
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
