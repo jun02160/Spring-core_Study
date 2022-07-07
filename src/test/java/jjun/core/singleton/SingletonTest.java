ffa5cf1a3e5e28ca9cd78f65e5704988cdbca356
jun <jun020216@naver.com>
2022-07-07 17:04:01 +0900

    Singleton Container

--- src/test/java/jjun/core/singleton/SingletonTest.java
+++ src/test/java/jjun/core/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import jjun.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -39,5 +40,27 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);   // 같은 인스턴스인지 비교
+
+        singletonService1.logic();
+        singletonService2.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 서로 다른 참조값이 생성된 것을 확인 -> 총 4개의 객체가 생성된 상태 => 하나만 생성해서 공유하는 방식으로 해결!
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
     }
 }
