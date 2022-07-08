0f5e9f5ccdabde174eb6120586fc04d6d61f0b70
jun <jun020216@naver.com>
2022-07-08 15:37:45 +0900

    Constructor Injection

---
+++ src/test/java/jjun/core/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package jjun.core.order;
+
+import jjun.core.discount.FixDiscountPolicy;
+import jjun.core.member.Grade;
+import jjun.core.member.Member;
+import jjun.core.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
