ffca7bc00a75ec29c19fbcce082856c9b4c94ddd
jun <jun020216@naver.com>
2022-07-12 16:14:53 +0900

    Scope Provider

---
+++ src/main/java/jjun/core/common/MyLogger.java
@@ -0,0 +1,36 @@
+package jjun.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;   // 다른 HTTP 요청과 구분하기 위한 아이디
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
